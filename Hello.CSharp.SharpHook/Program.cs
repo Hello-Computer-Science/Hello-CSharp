a29f9fc5bfa65453c5b35f620c1d41eaa4d7422a
Dynesshely <catrol@qq.com>
2023-05-26 18:55:36 +0800

    💾 Feat(SharpHook): Hook test.

---
+++ Hello.CSharp.SharpHook/Program.cs
@@ -0,0 +1,21 @@
+﻿using SharpHook;
+
+
+var hook = new TaskPoolGlobalHook();
+
+hook.KeyTyped += (_, args) =>
+{
+    Console.WriteLine($"Typed: {args.Data.KeyCode}: {args.Data.KeyChar}");
+};
+
+hook.KeyPressed += (_, args) =>
+{
+    Console.WriteLine($"Pressed: {args.Data.KeyCode}: {args.Data.KeyChar}");
+};
+
+hook.KeyReleased += (_, args) =>
+{
+    Console.WriteLine($"Released: {args.Data.KeyCode}: {args.Data.KeyChar}");
+};
+
+hook.RunAsync();
