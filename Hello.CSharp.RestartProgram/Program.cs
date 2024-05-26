86bbfa86004fb8d0ad98eacddfbc821af65f0939
Dynesshely <catrol@qq.com>
2024-05-26 14:35:41 +0800

    Test fastest url in url list

---
+++ Hello.CSharp.RestartProgram/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Reflection.Emit;
+
+Console.Write("Press enter to generate dynamic assembly ...");
+
+Console.ReadLine();
+
+var assemblyName = new AssemblyName("DynamicAssembly");
+var assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Save);
+
+var moduleBuilder = assemblyBuilder.DefineDynamicModule("DynamicModule", "DynamicAssembly.dll");
+
+var typeBuilder = moduleBuilder.DefineType("DynamicType", TypeAttributes.Public);
+
+var methodBuilder = typeBuilder.DefineMethod("DynamicMethod", MethodAttributes.Public | MethodAttributes.Static, typeof(void), null);
+
+var ilGenerator = methodBuilder.GetILGenerator();
+
+ilGenerator.Emit(OpCodes.Ldstr, "Hello, world!");
+ilGenerator.Emit(OpCodes.Ldstr, "output.txt");
+
+var writeLineMethod = typeof(File).GetMethod("AppendAllText", new[] { typeof(string) });
+ilGenerator.EmitCall(OpCodes.Call, writeLineMethod, null);
+
+ilGenerator.Emit(OpCodes.Ret);
+
+var dynamicType = typeBuilder.CreateType();
+
+assemblyBuilder.Save("DynamicAssembly.dll");
+
+AppDomain.CurrentDomain.ProcessExit += (sender, e) =>
+{
+    var dynamicAssembly = Assembly.LoadFrom("DynamicAssembly.dll");
+    var dynamicTypeLoaded = dynamicAssembly.GetType("DynamicType");
+    var dynamicMethodLoaded = dynamicTypeLoaded.GetMethod("DynamicMethod");
+    dynamicMethodLoaded.Invoke(null, null);
+};
+
+Console.Write("Press enter to restart the program ...");
+
+Console.ReadLine();
+
+Environment.Exit(0);
---
+++ Hello.CSharp.TestUrlDelay/Program.cs
@@ -0,0 +1,33 @@
+﻿var random = new Random();
+
+using var http = new HttpClient();
+
+var urls = new List<string> { "www.baidu.com", "www.google.com", "www.bilibili.com" };
+var queries = urls.Select(url => Task.Factory.StartNew(() => TestDelay(url))); // You can switch to `TestActualDelay` function
+var fastest = await await await Task.WhenAny(queries);
+
+Console.WriteLine($"{DateTime.Now:O} {fastest.Item2} ms -> Url {fastest.Item1}");
+
+return;
+
+async Task<(string, double)> TestDelay(string url)
+{
+    var delay = random.Next(1, 15) * 300;
+    Console.WriteLine($"{DateTime.Now:O} Testing {url} ... (It will took {delay} ms)");
+    for (var i = 0; i < 3; ++i) // This line with below 4 lines should be changed to GET method on url
+    {
+        await Task.Delay(delay / 3);
+        Console.WriteLine($"\t Test of {url} is still alive !");
+    }
+    return (url, delay);
+}
+
+async Task<(string, double)> TestActualDelay(string url)
+{
+    var begin = DateTime.Now;
+    Console.WriteLine($"{begin:O} Testing {url} ...");
+    var response = await http.GetAsync($"https://{url}");
+    var end = DateTime.Now;
+    
+    return (url, (begin - end).Milliseconds);
+}
